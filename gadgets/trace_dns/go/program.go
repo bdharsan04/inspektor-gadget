deb16b7d08f468f88b15dca5742309eb26232754
Dharsan B <147311516+bdharsan04@users.noreply.github.com>
2024-12-13 04:10:45 +0530

    Update program.go
    Signed-off-by: Dharsan B <147311516+bdharsan04@users.noreply.github.com>

--- gadgets/trace_dns/go/program.go
+++ gadgets/trace_dns/go/program.go
@@ -258,6 +258,8 @@ func gadgetInit() int {
 			case dnsmessage.TypeAAAA:
 				ipv6 := answer.Body.(*dnsmessage.AAAAResource)
 				str = net.IP(ipv6.AAAA[:]).String()
+			default:
+				continue
 			}
 			if str!= ""{
 				addresses = append(addresses, str)
